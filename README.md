a393ff463d25be164ad351f2eab2215edb908dc3
Garrett Holaday <holadaygarrett@gmail.com>
2021-09-16 17:17:23 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Haunt
+
+Developed with Unreal Engine 4
---
+++ Source/Haunt/HauntGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HauntGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HAUNT_API AHauntGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
