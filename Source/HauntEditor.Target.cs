fe06e777ca730bef20b11f82320beaaabe585229
Garrett Holaday <holadaygarrett@gmail.com>
2024-12-17 10:43:29 -0600

    update to 5.5, add basic speak gibberish

--- Source/HauntEditor.Target.cs
+++ Source/HauntEditor.Target.cs
@@ -8,7 +8,7 @@ public class HauntEditorTarget : TargetRules
 	public HauntEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 		ExtraModuleNames.AddRange( new string[] { "Haunt" } );
 	}
 }
